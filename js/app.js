3cb4f0771f46cd9175a39367290b4efbe34d1ada
Nirina RAZAKANIRINA <nirizakah@gmail.com>
2019-11-22 14:12:25 +0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,77 @@
+(function($){
+    // // Sticky menu 
+    // $(document).on("scroll", function(){
+    //     // var widthScreen = $( window ).width();
+    //     if($(document).scrollTop() > 1){
+    //         $(".header").addClass("sticky");
+    //     }else{
+    //         $(".header").removeClass("sticky");
+    //     }
+    // });
+    
+    
+    // menu mobile
+    $('#main-nav').hcOffcanvasNav({
+        insertClose: false,
+        labelBack: 'Back' // back buton
+    });
+
+    // slide sidebar
+    $('.slideService').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        centerPadding: '0px',
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+    });
+    
+    var valMsg = $('textarea.form-control').val();
+    $('textarea.form-control').on('focus', function(){
+        $('textarea.form-control').css('height', '150px');
+    }), 1000;
+
+    // menu mobile
+    $('#main-nav').hcOffcanvasNav({
+        insertClose: false,
+        labelBack: 'Back' // back buton
+    });
+    
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+    
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+
+})(jQuery);
